ff252380b6b7bdcedf021ab2dd9e7dcecbb86cd3
Nazier Abdurahman <nazierabdurahman2@gmail.com>
2024-08-02 00:00:43 +0200

    diag moves

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var screen_size : Vector2
-var speed = 400
+var speed = 200
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -17,15 +17,26 @@ func _physics_process(delta):
 		$AnimatedSprite2D.animation = "walk_right"
 		$AnimatedSprite2D.play()
 		position.x += speed * delta
+		if Input.is_action_pressed("up"):
+			position.y -= speed * delta
+		elif Input.is_action_pressed("down"):
+			position.y += speed * delta
+			
 	elif Input.is_action_pressed("left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.animation = "walk_left"
 		$AnimatedSprite2D.play()
 		position.x -= speed * delta
+		if Input.is_action_pressed("up"):
+			position.y -= speed * delta
+		elif Input.is_action_pressed("down"):
+			position.y += speed * delta
+			
 	elif Input.is_action_pressed("down"):
 		$AnimatedSprite2D.animation = "walk_down"
 		$AnimatedSprite2D.play()
 		position.y += speed * delta
+		
 	elif Input.is_action_pressed("up"):
 		$AnimatedSprite2D.animation = "walk_up"
 		$AnimatedSprite2D.play()
