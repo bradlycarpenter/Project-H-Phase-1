8bb4cbd16b4d20c20dc24c06760c0ad1474a7385
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-08-01 23:00:20 +0200

    Movement and Terrain V1

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,17 +2,12 @@ extends CharacterBody2D
 
 var screen_size : Vector2
 var speed = 400
-var input_direction = Input.get_vector("left", "right", "up", "down")
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = screen_size / 2
 
-
-
 func _physics_process(delta):
-
-	
 	#limit movement to window size
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
@@ -20,22 +15,21 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.animation = "walk_right"
+		$AnimatedSprite2D.play()
 		position.x += speed * delta
 	elif Input.is_action_pressed("left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.animation = "walk_left"
+		$AnimatedSprite2D.play()
 		position.x -= speed * delta
 	elif Input.is_action_pressed("down"):
 		$AnimatedSprite2D.animation = "walk_down"
+		$AnimatedSprite2D.play()
 		position.y += speed * delta
 	elif Input.is_action_pressed("up"):
 		$AnimatedSprite2D.animation = "walk_up"
-		position.y -= speed * delta
-	
-	#$AnimatedSprite2D.animation = "walk_right"
-	# Player animation
-	if Input.is_anything_pressed():
 		$AnimatedSprite2D.play()
+		position.y -= speed * delta
 	else:
 		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.frame = 0
