b653df94e638ced37806e00393d6ffe68e9aa5f0
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-08-01 22:55:50 +0200

    TileAdded

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,21 +1,17 @@
 extends CharacterBody2D
 
-@export var speed = 400
 var screen_size : Vector2
+var speed = 400
+var input_direction = Input.get_vector("left", "right", "up", "down")
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = screen_size / 2
 
-func get_input():
-	# keyboard input
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+
 
 func _physics_process(delta):
-	# Player Movement
-	get_input()
-	move_and_slide()
+
 	
 	#limit movement to window size
 	position = position.clamp(Vector2.ZERO, screen_size)
@@ -24,17 +20,21 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.animation = "walk_right"
+		position.x += speed * delta
 	elif Input.is_action_pressed("left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.animation = "walk_left"
+		position.x -= speed * delta
 	elif Input.is_action_pressed("down"):
 		$AnimatedSprite2D.animation = "walk_down"
+		position.y += speed * delta
 	elif Input.is_action_pressed("up"):
 		$AnimatedSprite2D.animation = "walk_up"
+		position.y -= speed * delta
 	
 	#$AnimatedSprite2D.animation = "walk_right"
 	# Player animation
-	if velocity.length() != 0:
+	if Input.is_anything_pressed():
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
