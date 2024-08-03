9422ea690fcfea9a6121585f92b19f87fe10679d
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-08-03 03:07:38 +0200

    Added Delta to Player Script

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,21 +1,17 @@
 extends CharacterBody2D
 
 var screen_size : Vector2
-var speed = 5
+var speed = 400
 var direction : Vector2
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = screen_size / 2
 
-func _movement():
+func _movement(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
-	position += velocity
-	if direction:
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+	velocity = direction * speed * delta
+	position += velocity 
 
 func _spriteDirection():
 	if (!$AnimatedSprite2D.flip_h and direction.x < 0):
@@ -24,16 +20,20 @@ func _spriteDirection():
 		$AnimatedSprite2D.flip_h = false
 
 func _spriteSelect():
-	if direction.y < 0:
-		$AnimatedSprite2D.animation = "walk_up"
-	elif direction.y > 0:
-		$AnimatedSprite2D.animation = "walk_down"
-	elif direction.x != 0:  # When moving horizontally
-		$AnimatedSprite2D.animation = "walk_right"
+	if direction:
+		$AnimatedSprite2D.play()
+		if direction.y < 0:
+			$AnimatedSprite2D.animation = "walk_up"
+		elif direction.y > 0:
+			$AnimatedSprite2D.animation = "walk_down"
+		elif direction.x != 0:
+			$AnimatedSprite2D.animation = "walk_right"
+	else:
+		$AnimatedSprite2D.stop()
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	#limit movement to window size
 	position = position.clamp(Vector2.ZERO, screen_size)
-	_movement()
+	_movement(delta)
 	_spriteDirection()
 	_spriteSelect()
