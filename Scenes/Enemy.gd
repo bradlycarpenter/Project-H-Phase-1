fe1da3d3e831482e7814f277e355e710908a9599
Nazier Abdurahman <nazierabdurahman2@gmail.com>
2024-08-03 18:32:10 +0200

    cleaned code

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -2,36 +2,44 @@ extends CharacterBody2D
 
 var speed = 85
 var player_chase =  false
-var Player = null
+var player = null
 var direction : Vector2
+var last_flip_h = false
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if player_chase:
-		# Move towards the player
-		position += (Player.position - position) / speed
-		
-		# Determine which animation to play
-		if abs(Player.position.x - position.x) > abs(Player.position.y - position.y):
-			# Horizontal movement
-			if Player.position.x < position.x:
-				$AnimatedSprite2D.play("walk_right")
-				$AnimatedSprite2D.flip_h = true  # Facing left
-			else:
-				$AnimatedSprite2D.play("walk_right")
-				$AnimatedSprite2D.flip_h = false  # Facing right
-		else:
-			# Vertical movement
-			if Player.position.y < position.y:
-				$AnimatedSprite2D.play("walk_up")
-			else:
-				$AnimatedSprite2D.play("walk_down")
+		_movement(delta)
+		_update_animation()
 	else:
 		$AnimatedSprite2D.stop()
 
+func _movement(delta):
+	if player:
+		position += (player.position - position).normalized()
+		velocity = direction * speed * delta
+		position += velocity 
+
+func _update_animation():
+	if player:
+		var new_flip_h = player.position.x < position.x
+		if abs(player.position.x - position.x) > abs(player.position.y - position.y):
+			# Horizontal movement
+			if new_flip_h != last_flip_h:
+				$AnimatedSprite2D.flip_h = new_flip_h
+				last_flip_h = new_flip_h
+			$AnimatedSprite2D.play("walk_right")
+		else:
+			# Vertical movement
+			if player.position.y < position.y:
+				$AnimatedSprite2D.play("walk_up")
+			else:
+				$AnimatedSprite2D.play("walk_down")
+
 func _on_detection_area_body_entered(body):
-	Player = body
+	player = body
 	player_chase = true
 
-func _on_detection_area_body_exited(_body):
-	Player = null
-	player_chase = false
+func _on_detection_area_body_exited(body):
+	if body == player:
+		player = null
+		player_chase = false
