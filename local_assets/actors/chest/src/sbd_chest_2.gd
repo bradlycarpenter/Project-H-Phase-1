8ca0777adb23935b11e4df95ff9e2f2f896b0cfd
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-08-19 11:11:40 +0200

    feat(chest): added chest
    - Requires "use" keybind to be added to be opened.
    - Opens when player enters area and "use" is pressed.
    - Requires coins to be added to have coins come out of it.

---
+++ local_assets/actors/chest/src/sbd_chest_2.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+var player_entered_area : bool = false
+var chest_is_open : bool = false
+
+@onready var ans_chest_2: AnimatedSprite2D = $ans_chest_2
+@onready var asp_chest_open: AudioStreamPlayer = $asp_chest_open
+
+func _on_are_2_body_entered(_body: Node2D) -> void:
+	player_entered_area = true
+
+func _on_are_2_body_exited(_body: Node2D) -> void:
+	player_entered_area = false
+	
+
+ #Add "Use" keybind before it can be opened.
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("p1_use"):
+		#if player_entered_area and not chest_is_open:
+			#ans_chest_2.play()
+			#asp_chest_open.play()
+			#chest_is_open = true
