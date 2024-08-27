3142d44056ab9f0304ca147e3783d7e8a24b700d
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-08-27 20:10:08 +0200

    update

--- global_assets/scripts/state_machine/idle_state.gd
+++
@@ -1,6 +0,0 @@
-extends State
-
-class_name IdleState
-
-func _ready() -> void:
-    animation_tree["parameters/conditions/idle"] = true
---
+++ local_assets/actors/enemy_chars/enemy_boss1/src/cbd_boss_2.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+class_name BossEnemy
+
+func _physics_process(delta) -> void:
+	move_and_slide()
+	
